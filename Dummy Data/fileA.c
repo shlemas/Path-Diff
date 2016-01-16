92bc0587ac9e4166f5c829902620f2a57fb0ca2a
Shawn LeMaster <shlemas@icloud.com>
2016-01-16 16:19:25 -0600

    Add a couple dummy test files

---
+++ Dummy Data/fileA.c
@@ -0,0 +1,9 @@
+int main()
+{
+    int a = 5;
+    int b = 4;
+
+    printf("%d + %d = %d", a, b, a + b);
+
+    return 0;
+}
---
+++ Dummy Data/fileB.c
@@ -0,0 +1,9 @@
+int main()
+{
+    int a = 5;
+    int b = 4;
+
+    fprintf(stderr, "%d - %d = %d", a, b, a - b);
+
+    return 0;
+}
